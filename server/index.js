434ce7135e66aa9b048ff30ccbcf3ea09f9fa99f
Axel <axel.monsalve.ponce@gmail.com>
2020-01-22 18:13:46 -0300

    First steps creating the api

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require('./config/db');
 
 //Crear el servidor
 const app = express();
+
+//Conectar a la base de datos
+connectDB();
 
 //Creamos un puerto para la app
 const PORT = process.env.PORT || 4000;
