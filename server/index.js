c932adb959f850ebda4f07dbb8830273b6823a3d
Axel <axel.monsalve.ponce@gmail.com>
2020-01-22 16:04:26 -0300

    Creating the Express Server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log('Desde index.js');
+const express = require("express");
+
+//Crear el servidor
+const app = express();
+
+//Creamos un puerto para la app
+const PORT = process.env.PORT || 4000;
+
+//Definir la página principal
+app.get('/', (req, res)=> {
+    res.send('Hola mundo');
+});
+
+//Arrancar la app o el servidor
+app.listen(PORT, () => {
+  console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
